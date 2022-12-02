50c7b0ad5e0540e55aec4a3cb6ff170048362d76
jkracz <joseph.kracz@gmail.com>
2022-12-01 18:37:21 -0800

    added some logs to debug the connection

--- server.js
+++ server.js
@@ -21,7 +21,13 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json("The API is working");
+  db.select('*').from('users')
+  .then(data => res.status(200).send(data))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json('no worky');
+  });
+  // res.json("The API is working");
 });
 
 app.get("/health", (req, res) => {res.sendStatus(200)});
